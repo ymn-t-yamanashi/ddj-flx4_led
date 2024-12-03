e0e3ca8e757249513b426129b65bfe48b4c46ee2
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-12-03 20:16:06 +0900

    Pad LED

--- lib/pad.ex
+++ lib/pad.ex
@@ -3,20 +3,43 @@ defmodule Pad do
   Documentation for `pad`.
   """
 
+  @led_on "7F"
+  @led_of "00"
+
+  @pad1 "00"
+  @pad2 "01"
+  @pad3 "02"
+  @pad4 "03"
+  @pad5 "04"
+  @pad6 "05"
+  @pad7 "06"
+  @pad8 "07"
+
+  @deck1 "97"
+  @deck2 "99"
+
+  @pad_list [@pad1, @pad2, @pad3, @pad4, @pad5, @pad6, @pad7, @pad8]
+
   def start() do
     {_, output} = PortMidi.open(:output, "DDJ-FLX4 MIDI 1")
 
-    Enum.each(1..10, fn _ -> led(output) end)
+    Enum.each(0..7, fn pad -> pad_led(output, pad) end)
     PortMidi.close(:output, output)
   end
 
-  def led(output) do
-    ["97", "00", "7f"]
+  def pad_led(output, pad) do
+    pad_hex = Enum.at(@pad_list, pad)
+    pad_led(output, @deck1, pad_hex)
+    pad_led(output, @deck2, pad_hex)
+  end
+
+  def pad_led(output, deck, pad) do
+    [deck, pad, @led_on]
     |> send_midi(output)
 
-    Process.sleep(100)
+    Process.sleep(500)
 
-    ["97", "00", "00"]
+    [deck, pad, @led_of]
     |> send_midi(output)
 
     Process.sleep(100)
